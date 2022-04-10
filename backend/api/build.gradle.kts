ac310aa852125d8099df1997266fa994e8208f0d
pinfort <ptg@nijitei.com>
2022-04-10 19:48:38 +0900

    change directory

---
+++ backend/api/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.6.20"
+}
+
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+}
---
+++ backend/settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "ts-videos-manager"
+include("api")
+include("console")
+include("infrastructure")
+include("front")
