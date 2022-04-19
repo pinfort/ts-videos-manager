3f62be1c34d1d0749deed78242b27353b65f56c8
pinfort <ptg@nijitei.com>
2022-04-19 22:39:54 +0900

    lint

--- backend/infrastructure/src/test/kotlin/me/pinfort/tsvideosmanager/infrastructure/database/mapper/ProgramMapperTest.kt
+++ backend/infrastructure/src/test/kotlin/me/pinfort/tsvideosmanager/infrastructure/database/mapper/ProgramMapperTest.kt
@@ -1,18 +1,13 @@
 package me.pinfort.tsvideosmanager.infrastructure.database.mapper
 
 import me.pinfort.tsvideosmanager.infrastructure.database.dto.ProgramDto
-import org.apache.ibatis.session.SqlSession
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.context.junit4.SpringRunner
 import org.testcontainers.junit.jupiter.Testcontainers
 import javax.sql.DataSource
 
@@ -28,7 +23,6 @@ class ProgramMapperTest {
 
     @BeforeEach
     fun setup() {
-
     }
 
     @Nested
@@ -37,15 +31,21 @@ class ProgramMapperTest {
         fun multiple() {
             val connection = dataSource.connection
             connection.prepareStatement("DELETE FROM program").execute()
-            connection.prepareStatement("""
+            connection.prepareStatement(
+                """
                 INSERT INTO program(id,name,executed_file_id,status) VALUES(1,'test',1,'REGISTERED');
-            """).execute()
-            connection.prepareStatement("""
+            """
+            ).execute()
+            connection.prepareStatement(
+                """
                 INSERT INTO program(id,name,executed_file_id,status) VALUES(2,'esta',2,'REGISTERED');
-            """).execute()
-            connection.prepareStatement("""
+            """
+            ).execute()
+            connection.prepareStatement(
+                """
                 INSERT INTO program(id,name,executed_file_id,status) VALUES(3,'aest',3,'REGISTERED');
-            """).execute()
+            """
+            ).execute()
             connection.commit()
 
             val actual = programMapper.selectByName("test")
