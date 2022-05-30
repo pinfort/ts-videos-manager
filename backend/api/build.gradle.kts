973db7761974bfec2fe14e324f39c28187b80186
pinfort <ptg@nijitei.com>
2022-05-31 01:26:44 +0900

    fix

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -2,16 +2,6 @@ dependencies {
     implementation(project(":infrastructure"))
     implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation(project(":infrastructure"))
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:mariadb")
-}
-
-extra["testcontainersVersion"] = "1.17.1"
-
-dependencyManagement {
-    imports {
-        mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
-    }
 }
 
 tasks.bootJar {
