18a88acd1d5e67310feae69af19931f268ae282b
pinfort <ptg@nijitei.com>
2022-05-31 01:16:52 +0900

    lint

--- backend/infrastructure/src/test/kotlin/me/pinfort/tsvideosmanager/infrastructure/database/mapper/ProgramMapperTest.kt
+++ backend/infrastructure/src/test/kotlin/me/pinfort/tsvideosmanager/infrastructure/database/mapper/ProgramMapperTest.kt
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
 import org.testcontainers.junit.jupiter.Testcontainers
 import javax.sql.DataSource
