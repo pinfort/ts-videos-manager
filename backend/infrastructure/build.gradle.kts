7ce1d16654908847d605166d0d53df749f301c4e
pinfort <ptg@nijitei.com>
2022-05-31 01:38:31 +0900

    fix

--- backend/infrastructure/build.gradle.kts
+++ backend/infrastructure/build.gradle.kts
@@ -11,8 +11,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     api("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
     testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:2.2.2")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:mariadb")
+    testApi("org.testcontainers:junit-jupiter")
+    testApi("org.testcontainers:mariadb")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 }
 
