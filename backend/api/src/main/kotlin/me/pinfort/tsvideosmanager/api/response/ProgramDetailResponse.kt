f717830babbaa0e7b7c261041ea3a3ecf303ab41
pinfort <ptg@nijitei.com>
2022-10-18 23:33:23 +0900

    add api

--- backend/api/src/main/kotlin/me/pinfort/tsvideosmanager/api/response/ProgramDetailResponse.kt
+++ backend/api/src/main/kotlin/me/pinfort/tsvideosmanager/api/response/ProgramDetailResponse.kt
@@ -1,7 +1,9 @@
 package me.pinfort.tsvideosmanager.api.response
 
+import me.pinfort.tsvideosmanager.infrastructure.structs.CreatedFile
 import me.pinfort.tsvideosmanager.infrastructure.structs.Program
 
 data class ProgramDetailResponse(
     val program: Program,
+    val videoFiles: List<CreatedFile>
 )
--- backend/infrastructure/src/main/kotlin/me/pinfort/tsvideosmanager/infrastructure/command/ProgramCommand.kt
+++ backend/infrastructure/src/main/kotlin/me/pinfort/tsvideosmanager/infrastructure/command/ProgramCommand.kt
@@ -1,8 +1,11 @@
 package me.pinfort.tsvideosmanager.infrastructure.command
 
 import me.pinfort.tsvideosmanager.infrastructure.database.dto.ProgramDto
+import me.pinfort.tsvideosmanager.infrastructure.database.dto.converter.CreatedFileConverter
 import me.pinfort.tsvideosmanager.infrastructure.database.dto.converter.ProgramConverter
+import me.pinfort.tsvideosmanager.infrastructure.database.mapper.CreatedFileMapper
 import me.pinfort.tsvideosmanager.infrastructure.database.mapper.ProgramMapper
+import me.pinfort.tsvideosmanager.infrastructure.structs.CreatedFile
 import me.pinfort.tsvideosmanager.infrastructure.structs.Program
 import org.springframework.stereotype.Component
 
@@ -10,6 +13,8 @@ import org.springframework.stereotype.Component
 class ProgramCommand(
     private val programMapper: ProgramMapper,
     private val programConverter: ProgramConverter,
+    private val createdFileMapper: CreatedFileMapper,
+    private val createdFileConverter: CreatedFileConverter,
 ) {
     fun selectByName(name: String, limit: Int = 100, offset: Int = 0): List<Program> {
         val programs: List<ProgramDto> = programMapper.selectByName(name, limit, offset)
@@ -19,5 +24,9 @@ class ProgramCommand(
 
     fun find(id: Int): Program? {
         return programMapper.find(id)?.let { programConverter.convert(it) }
+    }
+
+    fun videoFiles(program: Program): List<CreatedFile> {
+        return createdFileMapper.selectByExecutedFileId(program.executedFileId).map { createdFileConverter.convert(it) }
     }
 }
