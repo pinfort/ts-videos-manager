5ee6388e7cf75b9cb79b8c4a294f2d2b6c8a6738
pinfort <ptg@nijitei.com>
2023-06-09 22:53:49 +0900

    update

--- backend/console/src/main/kotlin/me/pinfort/tsvideosmanager/console/subcommands/Search.kt
+++ backend/console/src/main/kotlin/me/pinfort/tsvideosmanager/console/subcommands/Search.kt
@@ -3,15 +3,41 @@ package me.pinfort.tsvideosmanager.console.subcommands
 import kotlinx.cli.ArgType
 import kotlinx.cli.ExperimentalCli
 import kotlinx.cli.Subcommand
+import kotlinx.cli.required
+import me.pinfort.tsvideosmanager.console.component.TerminalTextColorComponent
+import me.pinfort.tsvideosmanager.infrastructure.command.ExecutedFileCommand
+import me.pinfort.tsvideosmanager.infrastructure.command.ProgramCommand
 import me.pinfort.tsvideosmanager.infrastructure.database.mapper.ProgramMapper
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalCli::class)
 class Search(
-    private val programMapper: ProgramMapper
+    private val programCommand: ProgramCommand,
+    private val executedFileCommand: ExecutedFileCommand,
+    private val terminalTextColorComponent: TerminalTextColorComponent,
 ) : Subcommand("search", "search programs") {
-    val programSearchName by option(ArgType.String, "name", "n", "ambiguous program name for search")
+    val datetimeFormat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
+
+    val programSearchName by option(ArgType.String, "name", "n", "ambiguous program name for search").required()
 
     override fun execute() {
-        println("test")
+        println("id\trecorded_at\t\tchannelName\t\tdrops\ttsExists\ttitle")
+
+        val programs = programCommand.selectByName(programSearchName, 500, 0)
+        programs.forEach {
+            val tsExists = programCommand.hasTsFile(it)
+            val executedFile = executedFileCommand.find(it.executedFileId) ?: return@forEach
+            val programInfo = "%d\t%s\t%s\t%d\t%b\t%s".format(it.id, datetimeFormat.format(executedFile.recordedAt), executedFile.channelName, it.drops ?: 0, tsExists, executedFile.title)
+            val decoratedProgramInfo = decorateProgramInfo(it.drops ?: -1, programInfo)
+            println(decoratedProgramInfo)
+        }
+    }
+
+    private fun decorateProgramInfo(drops: Int, programInfo: String): String {
+        return when {
+            drops == 0 -> programInfo
+            drops > 0 -> terminalTextColorComponent.error(programInfo)
+            else -> terminalTextColorComponent.info(programInfo)
+        }
     }
 }
