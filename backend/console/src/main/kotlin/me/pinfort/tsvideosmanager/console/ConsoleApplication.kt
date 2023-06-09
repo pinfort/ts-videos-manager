56a806732d7680b9245040ca08fcd896e743c1df
pinfort <ptg@nijitei.com>
2023-06-10 00:21:29 +0900

    springboot application config

--- backend/console/src/main/kotlin/me/pinfort/tsvideosmanager/console/ConsoleApplication.kt
+++ backend/console/src/main/kotlin/me/pinfort/tsvideosmanager/console/ConsoleApplication.kt
@@ -1,9 +1,19 @@
 package me.pinfort.tsvideosmanager.console
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = [
+        "me.pinfort.tsvideosmanager"
+    ]
+)
+@ConfigurationPropertiesScan(
+    basePackages = [
+        "me.pinfort.tsvideosmanager.infrastructure.config"
+    ]
+)
 class ConsoleApplication
 
 fun main(args: Array<String>) {
