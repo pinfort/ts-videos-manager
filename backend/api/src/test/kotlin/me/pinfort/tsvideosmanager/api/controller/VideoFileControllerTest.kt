65e66d479b6a4bc133250cd2323de6f4a227179a
pinfort <ptg@nijitei.com>
2023-07-12 22:00:04 +0900

    controller test update

--- backend/api/src/test/kotlin/me/pinfort/tsvideosmanager/api/controller/VideoFileControllerTest.kt
+++ backend/api/src/test/kotlin/me/pinfort/tsvideosmanager/api/controller/VideoFileControllerTest.kt
@@ -1,49 +1,37 @@
 package me.pinfort.tsvideosmanager.api.controller
 
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import me.pinfort.tsvideosmanager.infrastructure.command.CreatedFileCommand
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.testcontainers.containers.DockerComposeContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
-import java.io.File
+import java.io.InputStream
 
-@SpringBootTest(
-    properties = [
-        "samba.video-store-nas.url=smb://samba:139/alice",
-        "samba.video-store-nas.username=alice",
-        "samba.video-store-nas.password=alipass",
-        "samba.original-store-nas.url=smb://samba:139/bob",
-        "samba.original-store-nas.username=bob",
-        "samba.original-store-nas.password=bobpass",
-        "spring.datasource.url=jdbc:tc:mariadb:10.7:///?TC_INITSCRIPT=ddl/01_create_database.sql"
-    ]
-)
-@Testcontainers
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
 @AutoConfigureMockMvc
 class VideoFileControllerTest {
-    @Container
-    var dockerComposeContainer: DockerComposeContainer<*> =
-        DockerComposeContainer(listOf(File("src/test/resources/docker-compose.yml")))
-
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    @MockkBean
+    private lateinit var createdFileCommand: CreatedFileCommand
 
     @Nested
     inner class Stream {
         @Test
         fun success() {
+            every { createdFileCommand.streamCreatedFile(any()) } returns InputStream.nullInputStream()
+
             mockMvc.perform(get("/api/v1/video/1/stream"))
                 .andDo(print())
-                .andExpect(status().isNotFound)
+                .andExpect(status().isOk)
         }
     }
 }
