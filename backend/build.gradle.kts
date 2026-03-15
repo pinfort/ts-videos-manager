72fd96668e2464124cae4b39b7212b61fa6ffe6f
pinfort <ptg@nijitel.com>
2026-03-15 22:31:31 +0900

    update management

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("java-library")
     id("org.springframework.boot") version "2.6.6"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
